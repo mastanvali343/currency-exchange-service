0f716a45960c34ae62e0d9f71c707413bbe2a2c1
mastanvali343 <mastanvali343@gmail.com>
2023-12-25 22:55:01 +0530

    Added netflix eureka client

--- src/main/java/com/currencyexchange/CurrencyExchangeApplication.java
+++ src/main/java/com/currencyexchange/CurrencyExchangeApplication.java
@@ -2,7 +2,9 @@ package com.currencyexchange;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class CurrencyExchangeApplication {
 
