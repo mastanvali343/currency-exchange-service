9da24f1bb84eaae7cfd6be61f1fd998f2366311a
mastanvali343 <mastanvali343@gmail.com>
2023-12-28 13:24:51 +0530

    circuit breaker changes

--- README.md
+++ README.md
@@ -16,3 +16,10 @@ insert into
 currency_exchange(id,currency_from,currency_to,conversion_multiple,port) 
 values(1003,'AUD','INR',78,'');
 
+Circuit breaker
+==================
+1) for testing circuit breaker run the naming server and currency exchange service
+
+2) Take the curl commands from below file and past in windows power shell
+G:\workspace\ms2\currency-exchange-service\src\test\resources\api requests for circuit breaker.txt
+
--- src/main/java/com/currencyexchange/CircuitBreakerController.java
+++ src/main/java/com/currencyexchange/CircuitBreakerController.java
@@ -7,14 +7,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-import io.github.resilience4j.retry.annotation.Retry;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 
 @RestController
 public class CircuitBreakerController {
 	
 	private Logger logger = LoggerFactory.getLogger(CircuitBreakerController.class);
 	
-	@Retry(name = "sample-api", fallbackMethod = "fallbackMethod")
+	@CircuitBreaker(name = "sample-api", fallbackMethod = "fallbackMethod")
+//	@Retry(name = "sample-api", fallbackMethod = "fallbackMethod")
 //	@Retry(name = "sample-api")
 //	@Retry(name = "")
 	@GetMapping("sample-api")
@@ -24,10 +26,15 @@ public class CircuitBreakerController {
 		return response.getBody();
 	}
 	
+	@RateLimiter(name = "sample-api-rate")
+	@GetMapping("sample-api/ratelimit")
+	public String sampleApiForRateLimit() {
+		logger.info("======Inside sampleApi rate limit========");
+		return "sampleApi rate limit";
+	}
+	
 	public String fallbackMethod(Exception e) {
-		System.getenv().entrySet().stream().forEach(entry-> System.out.println(entry.getKey()+" ====> "+entry.getValue()));
-		System.out.println("================================================");
-		System.getProperties().entrySet().forEach(entry-> System.out.println(entry.getKey()+" ====> "+entry.getValue()));
+		
 		return "fall back method...";
 	}
 }
